df1721a32170a6bc43a62e81db62d8e7925b5b3c
Aman sharma <aman05382@gmail.com>
2020-11-24 17:46:21 +0530

    Update config_paytm.php

--- lib/config_paytm.php
+++ lib/config_paytm.php
@@ -8,8 +8,8 @@
 */
 
 define('PAYTM_ENVIRONMENT', 'TEST'); // PROD
-define('PAYTM_MERCHANT_KEY', 'ucOGuL4BnV#NuEfS'); //Change this constant's value with Merchant key received from Paytm.
-define('PAYTM_MERCHANT_MID', 'BvuNYX16485310423471'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_MERCHANT_KEY', '####################'); //Change this constant's value with Merchant key received from Paytm.
+define('PAYTM_MERCHANT_MID', '####################'); //Change this constant's value with MID (Merchant ID) received from Paytm.
 define('PAYTM_MERCHANT_WEBSITE', 'DEFAULT'); //Change this constant's value with Website name received from Paytm.
 
 $PAYTM_STATUS_QUERY_NEW_URL = 'https://securegw-stage.paytm.in/order/status';
